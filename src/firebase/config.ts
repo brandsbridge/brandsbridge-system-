7cb884ba80d125021d6b12eaafe820165fbc38cc
Brandsbridge LTD <info@brandsbridge.net>
2026-03-18 01:26:38 +0000

    You are an expert Next.js 15 and Firebase developer.
    TASK: Fix the Fire

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:479315293752:web:1e173bd184af1edba9d8cd",
   "apiKey": "AIzaSyCiDUpRZunrmRGFujXRfkvroHdxjJQAXyw",
   "authDomain": "brands-bridge.firebaseapp.com",
+  "storageBucket": "brands-bridge.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "479315293752"
 };
